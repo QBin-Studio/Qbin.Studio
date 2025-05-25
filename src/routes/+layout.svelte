bd46adcf46561dadf23400fe20c29c9b473c544c
MrBns <hi@mrbns.dev>
2025-05-26 01:53:48 +0600

    added metadata and favicon.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,13 @@
 <script lang="ts">
 	import Footer from '$lib/components/layout/Footer.svelte';
 	import Header from '$lib/components/layout/Header.svelte';
-	import '../app.css';
+	import '../tailwind.css';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<title>Qbin Studio - Bespoke Digital Experiences</title>
-	<meta
-		name="description"
-		content="Qbin Studio crafts premium, high-performance digital solutions through expert design, development, and strategy."
-	/>
-	<link
-		rel="icon"
-		href="/logos/SVG/dark-square.svg"
-	/>
+	<link rel="icon" href="/logos/SVG/dark-square.svg" />
 </svelte:head>
 <div class="flex min-h-screen flex-col">
 	<Header />
